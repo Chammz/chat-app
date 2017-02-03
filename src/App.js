503aebc7a72bb8d1c09517e2b5e508cd1be2d506
Tophs <chris@Chris.local>
2017-02-02 17:57:12 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-sidebar">
+          <h2>Chat App</h2>
+        </div>
+        <p className="App-body">
+          text goes here evenetually.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
