5f4f829dba9b36633dbcf6125b0ad1d5572c7b47
Tophs <chris@Chris.local>
2017-02-05 18:53:00 -0800

    Added Firebase SDK, looking into 404 issue

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+var admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert("/Users/chris/sites/bloc/chat-app/chat-app-f7523-firebase-adminsdk-fq7ol-1171517a46.json"),
+  databaseURL: "https://chat-app-f7523.firebaseio.com"
+});
