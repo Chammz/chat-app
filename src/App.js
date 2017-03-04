d4e2cac5ceda0740acf341473d21ae7e3c9c6080
Tophs <chris@Chris.local>
2017-03-04 10:17:17 -0800

    Added react bootstrap and implemented modal to create chat rooms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
 import React, { Component } from 'react';
 import './App.css';
 import {fetchRooms, anotherOne} from './lib/firebase.js'
+import {Modal, Button} from 'react-bootstrap';
 
+// looking into ES6 code school course
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {rooms: [], showModal: false}
+  }
+  componentDidMount() {
+    // fetch from firebase
+    this.fetchRoomData()
+  }
 
-  render() {
-    anotherOne()
-    fetchRooms().then(function(res) {
-      console.log(res)
+  // populates component state
+  fetchRoomData = () => {
+    fetchRooms().then((res) => {
+      // set state value as rooms
+      this.setState({rooms: res})
     })
+  }
 
+  closeModal = () => {
+    this.setState({ showModal: false });
+  }
+
+  openModal = () => {
+    this.setState({ showModal: true });
+  }
+
+  // what is this doing?
+  render() {
+    // grabbing keys from firebase object
+    // console.log(this.state)
+    const {rooms, showModal} = this.state
+    // const rooms = this.state.rooms
+    // const showModal = this.state.showModal
+    const roomKeys = Object.keys(rooms);
+    // console.log(this.state)
+    // const {showModal} = this.state
+
+    // room in this.state.rooms
+    // this.state.rooms.map
     return (
       <div className="App">
+        <div className="modal-container" style={{height: 200}}>
+        <Modal
+          show={showModal}
+          onHide={close}
+        >
+           <Modal.Header>
+             <Modal.Title>Modal title</Modal.Title>
+           </Modal.Header>
+
+           <Modal.Body>
+             One fine body...
+           </Modal.Body>
+
+           <Modal.Footer>
+             <Button onClick={this.closeModal}>Close</Button>
+             <Button bsStyle="primary">Save changes</Button>
+           </Modal.Footer>
+         </Modal>
+         </div>
         <div className="App-sidebar">
           <h2>Chat App</h2>
           <ul>
-            <li>Room 1</li>
-            <li>Room 2</li>
-            <li>Room 3</li>
+            {roomKeys.map((room) => (
+              <li>{this.state.rooms[room]}</li>
+            ))}
+
           </ul>
         </div>
         <p className="App-body">
