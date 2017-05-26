f3a807bee4932ca46f68680272702a530de49ab0
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-05-25 17:51:58 -0700

    css the things

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -2,10 +2,12 @@ import React from 'react'
 import ListMessage from './ListMessage'
 
 const Messages = ({mappedMessages}) => (
-  <div>
-    {
-      mappedMessages.length > 0 ? <ListMessage messages={mappedMessages} /> : 'Loading messages…'
-    }
+  <div className="Message">
+    <div className="listed-messages">
+      {
+        mappedMessages.length > 0 ? <ListMessage messages={mappedMessages} /> : 'Loading messages…'
+      }
+    </div>
   </div>
 )
 
